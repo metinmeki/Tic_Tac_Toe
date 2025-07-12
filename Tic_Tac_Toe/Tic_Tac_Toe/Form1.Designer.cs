e7f457ac82bc7f91d47e602dc8fe17e0d5594dea
Metin Amedi <123455084+metinmeki@users.noreply.github.com>
2025-07-12 08:42:46 +0300

    Add files via upload
    # Tic Tac Toe
    
    This is a simple Tic Tac Toe game built with C# and Windows Forms.
    
    ## How to Run
    
    - Open the project in Visual Studio or Visual Studio Code.
    - Build and run the application.
    - Play the game by clicking the buttons.
    
    ## Features
    
    - Two-player game (X and O).
    - Detects winners and draws.
    - Reset button to start a new game.
    
    ## License
    
    This project is open source and free to use.

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 76);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(113, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(95, 76);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(214, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(95, 76);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 94);
+            button4.Name = "button4";
+            button4.Size = new Size(95, 76);
+            button4.TabIndex = 5;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(113, 94);
+            button5.Name = "button5";
+            button5.Size = new Size(95, 76);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(214, 94);
+            button6.Name = "button6";
+            button6.Size = new Size(95, 76);
+            button6.TabIndex = 3;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 176);
+            button7.Name = "button7";
+            button7.Size = new Size(95, 76);
+            button7.TabIndex = 8;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(113, 176);
+            button8.Name = "button8";
+            button8.Size = new Size(95, 76);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(214, 176);
+            button9.Name = "button9";
+            button9.Size = new Size(95, 76);
+            button9.TabIndex = 6;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(314, 260);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form1.cs
@@ -0,0 +1,173 @@
+﻿namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] gameBord = new string[9];
+        int currenTurn = 0;
+
+        public string returnSymbol(int turn)
+        {
+            return (turn % 2 == 0) ? "X" : "O";
+        }
+
+        public void checkForWinner()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                string combination = "";
+
+                switch (i)
+                {
+                    case 0:
+                        combination = gameBord[0] + gameBord[4] + gameBord[8];
+                        break;
+                    case 1:
+                        combination = gameBord[2] + gameBord[4] + gameBord[6];
+                        break;
+                    case 2:
+                        combination = gameBord[0] + gameBord[1] + gameBord[2];
+                        break;
+                    case 3:
+                        combination = gameBord[3] + gameBord[4] + gameBord[5];
+                        break;
+                    case 4:
+                        combination = gameBord[6] + gameBord[7] + gameBord[8];
+                        break;
+                    case 5:
+                        combination = gameBord[0] + gameBord[3] + gameBord[6];
+                        break;
+                    case 6:
+                        combination = gameBord[2] + gameBord[4] + gameBord[7];
+                        break;
+                    case 7:
+                        combination = gameBord[1] + gameBord[4] + gameBord[7];
+                        break;
+                }
+
+                if (combination.Equals("OOO"))
+                {
+                    resetGame();
+                    MessageBox.Show("Player O wins!", "We have a winner!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                else if (combination.Equals("XXX"))
+                {
+                    resetGame();
+                    MessageBox.Show("Player X wins!", "We have a winner!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+        }
+
+        public void resetGame()
+        {
+            currenTurn = 0;
+            gameBord = new string[9];
+
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[0])) return;
+
+            currenTurn++;
+            gameBord[0] = returnSymbol(currenTurn);
+            button1.Text = gameBord[0];
+            checkForWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[1])) return;
+
+            currenTurn++;
+            gameBord[1] = returnSymbol(currenTurn);
+            button2.Text = gameBord[1];
+            checkForWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[2])) return;
+
+            currenTurn++;
+            gameBord[2] = returnSymbol(currenTurn);
+            button3.Text = gameBord[2];
+            checkForWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[3])) return;
+
+            currenTurn++;
+            gameBord[3] = returnSymbol(currenTurn); // ✅ FIXED
+            button4.Text = gameBord[3];
+            checkForWinner();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[4])) return;
+
+            currenTurn++;
+            gameBord[4] = returnSymbol(currenTurn);
+            button5.Text = gameBord[4];
+            checkForWinner();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[5])) return;
+
+            currenTurn++;
+            gameBord[5] = returnSymbol(currenTurn);
+            button6.Text = gameBord[5];
+            checkForWinner();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[6])) return;
+
+            currenTurn++;
+            gameBord[6] = returnSymbol(currenTurn); // ✅ FIXED
+            button7.Text = gameBord[6];
+            checkForWinner();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[7])) return;
+
+            currenTurn++;
+            gameBord[7] = returnSymbol(currenTurn);
+            button8.Text = gameBord[7];
+            checkForWinner();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(gameBord[8])) return;
+
+            currenTurn++;
+            gameBord[8] = returnSymbol(currenTurn);
+            button9.Text = gameBord[8];
+            checkForWinner();
+        }
+    }
+}
